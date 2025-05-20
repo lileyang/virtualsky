6b57cf043b2c0fe093ed1ec68d88a85286c34c61
李乐洋 <13864172818@139.com>
2025-05-20 09:11:46 +0800

    跳转中转页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import AppPage from './components/AppPage.vue'
 import 'normalize.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// 创建 axios 实例（可选）
+const axiosInstance = axios.create({
+  baseURL: 'http://172.16.0.93', // 替换为你的接口地址
+  timeout: 10000
+})
+
+Vue.use(VueAxios, axiosInstance) 
 
 import router from './router/index.js'
 
