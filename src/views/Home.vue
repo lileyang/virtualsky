a5e4d862f57a6ce519c89b69c7894c5f144f31aa
李乐洋 <13864172818@139.com>
2025-05-27 15:30:15 +0800

    增加墨多多 2，3集

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="body">
     <!-- <img src="@/assets/img/img_welcome.jpg" class="welcome-img" alt=""> -->
-    <div id="carouselExampleInterval" class="carousel slide"  data-bs-ride="carousel">
+    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="true">
       <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="3000">
+        <div class="carousel-item active" data-bs-interval="2000">
+          <img src="@/assets/img/wel3.jpg" class="d-block w-100">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
           <img src="@/assets/img/wel1.jpg" class="d-block w-100">
         </div>
         <div class="carousel-item" data-bs-interval="3000">
           <img src="@/assets/img/wel2.jpg" class="d-block w-100">
         </div>
-        <div class="carousel-item" data-bs-interval="3000">
-          <img src="@/assets/img/wel3.jpg" class="d-block w-100">
-        </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
     <div class="channel" v-for="(channel, channelIndex) in channels" :key="channelIndex">
       <div class="lable">
